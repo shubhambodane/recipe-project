8212cae6f662dde40a994bb5d111d73918f10f68
Shubham Bodane <shubham.bodane@siemens.com>
2024-02-15 22:33:19 +0530

    shopping-list component

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('banana', 100),
+    new Ingredient('apple', 200),
+  ];
+}
